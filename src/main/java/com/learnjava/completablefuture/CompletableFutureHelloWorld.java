95d426c8c356fafd441626003a43ddb31382ea93
Young wook Jeon <ela87@naver.com>
2020-11-21 13:39:25 +0900

    Add some methods using CompletableFuture api

--- src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
+++ src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -4,38 +4,100 @@ import com.learnjava.service.HelloWorldService;
 
 import java.util.concurrent.CompletableFuture;
 
-import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.CommonUtil.*;
 import static com.learnjava.util.LoggerUtil.log;
 
 public class CompletableFutureHelloWorld {
 
-    private HelloWorldService helloWorldService;
+    private HelloWorldService hws;
 
     public CompletableFutureHelloWorld(HelloWorldService helloWorldService) {
-        this.helloWorldService = helloWorldService;
+        this.hws = helloWorldService;
     }
 
     public CompletableFuture<String> helloWorld() {
-        return CompletableFuture.supplyAsync(helloWorldService::helloWorld)
+
+        return CompletableFuture.supplyAsync(() -> hws.helloWorld())//  runs this in a common fork-join pool
                 .thenApply(String::toUpperCase);
     }
 
     public CompletableFuture<String> helloWorld_withSize() {
-        return CompletableFuture.supplyAsync(helloWorldService::helloWorld)
+
+        return CompletableFuture.supplyAsync(() -> hws.helloWorld())//  runs this in a common fork-join pool
                 .thenApply(String::toUpperCase)
-                .thenApply(s -> s.length() + "-" + s);
+                .thenApply((s) -> s.length() + "-" + s);
     }
 
-    public static void main(String[] args) {
-        HelloWorldService helloWorldService = new HelloWorldService();
+    public String helloWorld_multiple_async_calls() {
+        startTimer();
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> this.hws.hello());
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(() -> this.hws.world());
 
-        CompletableFuture.supplyAsync(helloWorldService::helloWorld)
+        String hw = hello
+                .thenCombine(world, (h, w) -> h + w) // (first,second)
                 .thenApply(String::toUpperCase)
-                .thenAccept(result -> {
-                    log("Result is " + result);
-                })
                 .join();
-        log("Done!");
-        // delay(2000); we don't need this
+
+        timeTaken();
+
+        return hw;
+    }
+
+
+    public String helloWorld_3_async_calls() {
+        startTimer();
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> this.hws.hello());
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(() -> this.hws.world());
+        CompletableFuture<String> hiCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return " HI CompletableFuture!";
+        });
+
+        String hw = hello
+                .thenCombine(world, (h, w) -> h + w) // (first,second)
+                .thenCombine(hiCompletableFuture, (previous, current) -> previous + current)
+                .thenApply(String::toUpperCase)
+                .join();
+
+        timeTaken();
+
+        return hw;
+    }
+
+    public String helloWorld_4_async_calls() {
+        startTimer();
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> this.hws.hello());
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(() -> this.hws.world());
+        CompletableFuture<String> hiCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return " HI CompletableFuture!";
+        });
+        // Add the 4th CompletableFuture that returns a String "  Bye!"
+        CompletableFuture<String> bye = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return "  Bye!";
+        });
+
+        String hw = hello
+                .thenCombine(world, (h, w) -> h + w) // (first,second)
+                .thenCombine(hiCompletableFuture, (previous, current) -> previous + current)
+                // Combine the fourth CompletableFuture
+                .thenCombine(bye, (prev, cur) -> prev + cur)
+                .thenApply(String::toUpperCase)
+                .join();
+
+        timeTaken();
+
+        return hw;
+    }
+
+    public CompletableFuture<String> helloWorld_thenCompose() {
+
+        CompletableFuture<String> helloWorldFuture = CompletableFuture.supplyAsync(() -> this.hws.hello())
+                .thenCompose(previous -> hws.worldFuture(previous))
+                //.thenApply(previous -> helloWorldService.worldFuture(previous))
+                .thenApply(String::toUpperCase);
+
+        return helloWorldFuture;
     }
 }
--- src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldTest.java
+++ src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompletableFutureHelloWorldTest {
@@ -26,5 +28,37 @@ class CompletableFutureHelloWorldTest {
 
         completableFuture.thenAccept(s -> assertEquals("11-HELLO WORLD", s))
                 .join();
+    }
+
+    @Test
+    void helloWorld_multiple_async_calls() {
+        String helloWorld = cfhw.helloWorld_multiple_async_calls();
+
+        assertEquals("HELLO WORLD!", helloWorld);
+    }
+
+    @Test
+    void helloWorld_3_async_calls() {
+        String helloWorld = cfhw.helloWorld_3_async_calls();
+
+        assertEquals("HELLO WORLD! HI COMPLETABLEFUTURE!", helloWorld);
+    }
+
+    @Test
+    void helloWorld_thenCompose() {
+        // [Test worker] - Total Time Taken : 2052
+        startTimer();
+        CompletableFuture<String> completableFuture= cfhw.helloWorld_thenCompose();
+
+        completableFuture.thenAccept(s -> assertEquals("HELLO WORLD!", s))
+                .join();
+        timeTaken();
+    }
+
+    @Test
+    void helloWorld_4_async_calls() {
+        String helloWorld = cfhw.helloWorld_4_async_calls();
+
+        assertEquals("HELLO WORLD! HI COMPLETABLEFUTURE!  BYE!", helloWorld);
     }
 }
